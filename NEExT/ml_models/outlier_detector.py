c700d6accea5072d212d878c5ae34d4ebcbdc039
quak <kamilkulesza5@gmail.com>
2025-03-15 23:49:11 +0100

    Outlier dataset class

--- NEExT/ml_models/outlier_detector.py
+++ NEExT/ml_models/outlier_detector.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
@@ -7,28 +8,33 @@ from sklearn.preprocessing import StandardScaler
 from NEExT.collections.graph_collection import GraphCollection
 from NEExT.embeddings.embeddings import Embeddings
 
-
-class OutlierDetector(BaseEstimator):
+class OutlierDataset:
     def __init__(
         self,
         graph_collection: GraphCollection,
         embedding: Embeddings,
-        top_k: int = 10,
         standardize: bool = False,
-        threshold: float = 0.0,
     ):
         self.graph_collection = graph_collection
         self.data_df = embedding.embeddings_df
-        self.top_k = top_k
+        self.feature_cols = [col for col in self.data_df.columns if col not in ["graph_id"]]
         self.standardize = standardize
-        self.threshold = threshold
-
+        
+        if self.standardize:
+            self.scaler = StandardScaler()
+            self.data_df[self.feature_cols] = self.scaler.fit_transform(self.data_df[self.feature_cols])
+        
         self.labels_df = self._prepare_labels_df()
 
         # Merge embeddings with labels
         self.data_df = pd.merge(self.data_df, self.labels_df, on="graph_id")
-        self.unlabeled = self.data_df.query("label == -1")["graph_id"].to_list()
-        self.feature_cols = [col for col in self.data_df.columns if col not in ["graph_id", "label"]]
+        self.unlabeled_graphs = self.data_df.query("label == -1")["graph_id"].to_list()
+        self.X = self.data_df[self.feature_cols].values
+        self.y = self.data_df["label"].values
+        
+        self.X_unlabeled = self.data_df.query("label == -1")[self.feature_cols].values
+        if self.standardize:
+            self.X_unlabeled = self.scaler.transform(self.data_df.query("label == -1")[self.feature_cols])
 
     def _prepare_labels_df(self) -> pd.DataFrame:
         """
@@ -46,48 +52,53 @@ class OutlierDetector(BaseEstimator):
 
         return pd.DataFrame({"graph_id": graph_ids, "label": graph_labels})
 
-    def _vector_prediction(self, vector):
-        vectors = self.data_df[self.feature_cols].values
-        if self.standardize:
-            scaler = StandardScaler()
-            vectors = scaler.fit_transform(vectors)
-            vector = scaler.transform(vector.reshape(1, -1)).reshape(-1)
 
-        similarities = cosine_similarity(vectors, [vector]).reshape(-1)
+class OutlierDetector(BaseEstimator):
+    def __init__(
+        self,
+        top_k: int = 10,
+        threshold: float = 0.0,
+    ):
+        self.top_k = top_k
+        self.threshold = threshold
 
-        ind = np.argpartition(similarities, -self.top_k)[-self.top_k - 1 : -1]
-        similar_labels = self.data_df.loc[ind, "label"].values
-        similar_labels = np.array([i for i in similar_labels if i != -1])
+    def _vector_prediction(self, vector: np.ndarray):
+        similarities = cosine_similarity(self.vectors, [vector]).reshape(-1)
+
+        ind = np.argpartition(similarities, -self.top_k)[-self.top_k:]
+        similar_labels = np.array([i for i in self.labels[ind] if i != -1])
         return np.mean(similar_labels)
 
-    def fit(self, X=None, y=None):
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        self.vectors = X
+        self.labels = y
         return self
 
-    def predict_prob(self, X=None):
+    def predict_prob(self, X: np.ndarray):
         probs = []
 
-        for unlabeled_id in self.unlabeled:
-            vector = self.data_df.query(f"graph_id=={unlabeled_id}")[self.feature_cols].values.flatten()
+        for unlabeled_id in range(len(X)):
+            vector = X[unlabeled_id, :]
             prob = self._vector_prediction(vector)
             probs.append(prob)
 
         return np.array(probs)
 
-    def predict(self, X=None, probs=None):
+    def predict(self, X: np.ndarray, probs:Optional[np.ndarray]=None):
         if probs is None:
-            probs = self.predict_prob()
+            probs = self.predict_prob(X)
         preds = np.where(probs > self.threshold, 1, 0)
         return preds
 
-    def predict_full_df(self, X=None):
+    def predict_full_df(self, unlabeled: np.ndarray, X: np.ndarray):
         df = []
 
-        probs = self.predict_prob()
-        preds = self.predict(probs=probs)
+        probs = self.predict_prob(X)
+        preds = self.predict(X, probs=probs)
 
         df = pd.DataFrame(
             {
-                "graph_id": self.unlabeled,
+                "graph_id": unlabeled,
                 "prob": probs,
                 "pred": preds,
             }
