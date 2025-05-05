170ea946917dbcc6f588318e476d4a84e55f1244
quak <kamilkulesza5@gmail.com>
2025-05-05 20:32:25 +0200

    eXP 5

--- experiments/egonets/parameters/exp5.py
+++ experiments/egonets/parameters/exp5.py
@@ -18,14 +18,15 @@ k_and_length = [(1, 1), (2, 1), (2, 2)]
 
 params = [
     Param(
-        comment=f"{experiment}_{k}_{i}_{strategy}",
+        comment=f"{strategy}_local_{k_hop}_{local_i}_global_{global_i}",
         global_structural_feature_list=['all'],
         global_feature_vector_length=2,
         local_structural_feature_list=features,
-        local_feature_vector_length=i,
-        egonet_k_hop=k,
+        local_feature_vector_length=local_i,
+        egonet_k_hop=k_hop,
         embeddings_strategy=strategy,
     )
-    for k, i in k_and_length
+    for k_hop, local_i in k_and_length
     for strategy in ['separate_embeddings', 'combined_embeddings']
+    for global_i in [1, 2]
 ]
