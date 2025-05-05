82a8ee820ffbc990e0f2d9fa8707b7007346980a
quak <kamilkulesza5@gmail.com>
2025-05-05 21:05:55 +0200

    Expertiment 6

--- experiments/egonets/parameters/exp6.py
+++ experiments/egonets/parameters/exp6.py
@@ -1,7 +1,8 @@
+from NEExT.graphs import egonet
 from template import Param
 
 
-experiment = "embedding_combinations"
+experiment = "tuning_positional_encoding"
 
 features = [
     "degree_centrality",
@@ -14,19 +15,23 @@ features = [
 ]
 
 k_and_length = [(1, 1), (2, 1), (2, 2)]
+k_and_length = [(1, 1)]
 
 
 params = [
     Param(
-        comment=f"{strategy}_local_{k_hop}_{local_i}_global_{global_i}",
+        comment=f"{strategy}_local_{k_hop}_{local_i}_global_{global_i}_position_{one_hot}",
         global_structural_feature_list=['all'],
         global_feature_vector_length=global_i,
         local_structural_feature_list=features,
         local_feature_vector_length=local_i,
         egonet_k_hop=k_hop,
         embeddings_strategy=strategy,
+        egonet_position=True,
+        position_one_hot=one_hot,
     )
     for k_hop, local_i in k_and_length
-    for strategy in ['separate_embeddings', 'combined_embeddings']
-    for global_i in [1, 2]
+    for strategy in ['combined_embeddings']
+    for global_i in [2]
+    for one_hot in [False, True]
 ]
