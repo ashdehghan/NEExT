873a90911500e1ecc3eca59866545a4db693a0b6
quak <kamilkulesza5@gmail.com>
2025-06-03 23:14:09 +0200

    Update experiment parameters

--- experiments/outlier_detection/main.py
+++ experiments/outlier_detection/main.py
@@ -12,12 +12,13 @@ def prepare_dataset(data: str):
     edges_df, mapping_df, features_df, _ = load_abcdo_data(data, hide_frac={0: 0, 1: 0})
     community_id = features_df["community_id"]
     features_df = features_df.drop(columns=["random_community_feature", "community_id"])
-    return edges_df,mapping_df,features_df,community_id
+    return edges_df, mapping_df, features_df, community_id
 
 
 def main():
     workers = 4
     data = "abcdo_data_1000_200_0.3"
+    # default models
     params = [
         Params(
             comment="random_model",
@@ -27,25 +28,81 @@ def main():
             comment="node2vec",
             n2v=True,
         ),
+    ]
+    # just ego node features
+    params += [
         Params(
-            comment="neext_global_all_short",
-            global_structural_feature_list=["all"],
+            comment=f"node_features_{feature}",
+            global_structural_feature_list=[feature],
             global_feature_vector_length=1,
-            embeddings_strategy="feature_embeddings",
-        ),
+            embeddings_strategy="only_egonet_node_features",
+            egonet_k_hop=0,
+        )
+        for feature in ["all", "betastar", "page_rank", "degree_centrality", "clustering_coefficient"]
+    ]
+
+    # global models
+    MAX_K_HOP = 3
+
+    params += [
         Params(
-            comment="neext_global_all_long",
-            global_structural_feature_list=["all"],
-            global_feature_vector_length=5,
+            comment=f"global_structural_features_{feature}_{k_hop}_{vec_len}",
+            global_structural_feature_list=[feature],
+            global_feature_vector_length=vec_len,
             embeddings_strategy="feature_embeddings",
-        ),
+            egonet_k_hop=k_hop,
+        )
+        for feature in [
+            "all",
+            "degree_centrality",
+            "betastar",
+            "load_centrality",
+            "lsme",
+            "local_efficiency",
+            "betweenness_centrality",
+            "closeness_centrality",
+        ]
+        for k_hop in range(1, MAX_K_HOP + 1)
+        for vec_len in range(1, k_hop + 1)
+    ]
+
+    # local node features
+    features = [
+        "degree_centrality",
+        "betastar",
+        "load_centrality",
+        "lsme",
+        "local_efficiency",
+        "betweenness_centrality",
+        "closeness_centrality",
+    ]
+
+    k_and_length = [(1, 1), (2, 1), (2, 2)]
+
+    params += [
         Params(
-            comment="neext_local_betastar_short",
-            local_structural_feature_list=["betastar"],
-            local_feature_vector_length=1,
-            egonet_k_hop=1,
+            comment=f"local_features_{k}_{i}",
+            global_structural_feature_list=[],
+            local_structural_feature_list=features,
+            local_feature_vector_length=i,
+            egonet_k_hop=k,
             embeddings_strategy="structural_embeddings",
-        ),
+        )
+        for k, i in k_and_length
+    ]
+    params += [
+        Params(
+            comment=f"{strategy}_local_{k_hop}_{local_i}_global_{global_i}",
+            global_structural_feature_list=features,
+            global_feature_vector_length=global_i,
+            local_structural_feature_list=features,
+            local_feature_vector_length=local_i,
+            egonet_k_hop=k_hop,
+            embeddings_strategy=strategy,
+        )
+        for k_hop, local_i in k_and_length
+        for strategy in ["separate_embeddings", "combined_embeddings"]
+        for global_i in [1, 2]
     ]
 
     mlflow.set_tracking_uri(uri="http://127.0.0.1:5001")
